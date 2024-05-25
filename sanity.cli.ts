250d67ae44b16afa2d291319fcba34d6708a5788
serkfi <sebastian@serk.fi>
2024-05-25 23:05:43 +0300

    Fix missing projectId

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,9 +1,9 @@
 import { defineCliConfig } from "sanity/cli";
 
-import { dataset, projectId } from "./constants/sanityVariables";
+import { dataset } from "./constants/sanityVariables";
 
 export default defineCliConfig({
-  api: { projectId, dataset },
+  api: { projectId: "kj6xg0lj", dataset },
   project: {
     basePath: "/studio",
   },
